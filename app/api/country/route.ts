a75df024dc753de5c5b26b3b1106638fa380507c
hai.nguyen <nguyenminhhai0611@gmail.com>
2025-06-18 10:07:11 +0700

    define const runtime

--- app/api/country/route.ts
+++ app/api/country/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+export const runtime = 'edge';
 
 export async function GET(request: NextRequest) {
   const country = request.headers.get('x-vercel-ip-country')
